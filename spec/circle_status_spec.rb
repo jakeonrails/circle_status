0f7446e186972bfd2a92f315c0c1c713bc453144
Jake Moffatt <jakeonrails@gmail.com>
2016-01-08 17:41:55 -0800

    Move script code to Script class

--- spec/circle_status_spec.rb
+++ spec/circle_status_spec.rb
@@ -4,8 +4,4 @@ describe CircleStatus do
   it 'has a version number' do
     expect(CircleStatus::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
---
+++ spec/lib/circle_status/script_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe CircleStatus::Script do
+end
